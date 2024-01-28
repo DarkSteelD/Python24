1dd2d35cb8a0074d27f06019e27fb317ab58c3a6
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-01-28 12:20:39 +0300

    Update v3.py

--- v3.py
+++ v3.py
@@ -1,9 +1,21 @@
 #v3
 def MAXP(a):
-    for i in range(2,int(sqrt(a))):
-        if a % i == 0:
-            return a
+    for i in range(a,1,-1):
+        if a % i == 0 and ISP(i):
+            return i
     return 0
+def ISP(a):
+    for i in range(2,int(a**0.5)+1):
+        if a % i == 0 and a != 2:
+            return 0
+    return a
+#def MAXP2(a):
+    # for i in range(2,int(sqrt(a))):
+    #     if a % i == 0:
+    #         return a
+    # return 0
+#def SQRTNOLIB(a):
+    #later
 def P5(a):
     b = 1
     for i in range(2,int(sqrt(a))):
@@ -17,9 +29,14 @@ def MAXPNT( a):
     return 0
 def ATR(a):
     b = string(a)
-    a = 0
+    a = 1
     for i in range(0,b.size()):
-        
+        a *= int(b[i])
+    return a
 
 def NOD(a):
     return MAXPNT(a)*ATR(a)
+
+a = int(input("Введите число: "))
+a = MAXP(a)
+print("Максимальный простой делитель числа: ", a)
