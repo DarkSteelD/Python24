087c77975c7e02c643304f7a9f91a35edb12260c
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-02-10 17:55:30 +0300

    Create V3.14.py

---
+++ V3.14.py
@@ -0,0 +1,18 @@
+glas = "aeiouAEIOUаеёиоуыэюяАЕЁИОУЫЭЮЯ"
+soglas = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZбвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ"
+def pairs(s):
+    gs, sg = 0, 0
+    for i in range(len(s) - 1):
+        if s[i] in glas and s[i+1] in soglas:
+            gs += 1
+        elif s[i] in soglas and s[i+1] in glas:
+            sg += 1
+    return gs, sg
+def sort(s):
+    def difference(c):
+        gs, sg = pairs(c)
+        return abs(gs - sg)**2  
+    return sorted(s, key=difference)
+a = input("Введите строку: ")
+a1 = a.split('.')
+print(sort(a1))
