5afb9890725a6d570c5f93e4dbc4dcc6f735e487
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-02-10 23:10:33 +0300

    Update v15.1-2.py

--- LP3/v15.1-2.py
+++ LP3/v15.1-2.py
@@ -1,3 +1,23 @@
+import math
 class Shape:
     def __init__(self, identifier):
         self.identifier = identifier
+class Rectangle(Shape):
+    def __init__(self, identifier, top_left, bottom_right):
+        super().__init__(identifier)
+        self.top_left = top_left
+        self.bottom_right = bottom_right
+
+    def area(self):
+        width = self.bottom_right[0] - self.top_left[0]
+        height = self.top_left[1] - self.bottom_right[1]
+        return width * height
+
+class Pentagon(Shape):
+    def __init__(self, identifier, center, radius):
+        super().__init__(identifier)
+        self.center = center
+        self.radius = radius
+    def area(self):
+        side = 2 * self.radius * math.sin(math.pi / 5)
+        return 0.25 * math.sqrt(5 * (5 + 2 * math.sqrt(5))) * side ** 2
