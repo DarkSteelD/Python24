fd0340470a77aa202930844b6f8ad0f9c3e21644
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-02-14 14:37:57 +0300

    Update V5.py

--- LP5/V5.py
+++ LP5/V5.py
@@ -4,6 +4,7 @@
 # исключение о некорректном аргументе, иначе возвращает почтовый индекс
 # (Zip Code).
 import re
-a = input("Введите строку: ")
-a = re.match(r'\d{5}', a)
-print(True if a!= None else False)
+def check():
+    a = input("Введите строку: ")
+    a = re.match(r'\d{5}', a)
+    print(True if a!= None else False)
