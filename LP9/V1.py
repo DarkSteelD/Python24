d4e60303cd9c3ebc9a4c20c564d83af38cc121b8
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-02-14 15:52:06 +0300

    Update V1.py

--- LP9/V1.py
+++ LP9/V1.py
@@ -1,4 +1,5 @@
-with open('eggs.csv', newline='') as csvfile:
+import csv
+with open('C:\\Users\\DarkStell\\Documents\\GitHub\\Python24\\LP9\\15 - 1.csv', newline='',encoding='utf-8') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
         print(', '.join(row))
