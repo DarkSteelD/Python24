46589c9c58b17ec51c6ae158218bed145e62c431
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-02-14 11:50:39 +0300

    Update v5_1.py

--- LP4/v5_1.py
+++ LP4/v5_1.py
@@ -2,7 +2,12 @@ a = (int(f) for f in input(file="27-166a.txt".split()))
 counter = 0
 a1 = {}
 for l in range(2,a[0]):
+    counter %= a[1]
     if a1.get(counter,None) != None:
         a1[counter] = (a[l],0,0)
     elif min(a1[counter]) < a[l]:
-        a1[a1.get index(min(a1[counter])] = a[l]
+        a1[a1.get_index(min(a1[counter]))] = a[l]
+sum = ()
+for _ in range(a[1]):
+    sum[i] = a[_][0]+a[_][1]+a[_][2]
+print(sum.index(max(sum)))
