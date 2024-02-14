f4cb0c792b5b6076fe79ab4bd1b39fe12f06db3e
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-02-14 12:33:26 +0300

    Test

--- LP4/v5_1.py
+++ LP4/v5_1.py
@@ -1,13 +1,18 @@
-a = (int(f) for f in input(file="27-166a.txt".split()))
+with open("C:\\Users\\DarkStell\\Documents\\GitHub\\Python24\\LP4\\27-167a.txt") as file:
+    a = list((int(f) for f in file.read().split()))
 counter = 0
 a1 = {}
-for l in range(2,a[0]):
+for l in range(2,a[0]   ):
     counter %= a[1]
-    if a1.get(counter,None) != None:
-        a1[counter] = (a[l],0,0)
+    if counter not in a1:
+        a1[counter] = [a[l],0,0]
     elif min(a1[counter]) < a[l]:
-        a1[a1.get_index(min(a1[counter]))] = a[l]
-sum = ()
-for _ in range(a[1]):
-    sum[i] = a[_][0]+a[_][1]+a[_][2]
-print(sum.index(max(sum)))
+        a1[counter][a1[counter].index(min(a1[counter]))] = a[l]
+    counter +=1
+summer = []
+for i in range(a[1]):
+    if i in a1:
+        summer.append(sum(a1[i]))
+    else:
+        summer.append(0) 
+print(max(summer))
