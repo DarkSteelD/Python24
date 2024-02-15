acd2b74e99133ef19d4b1c888710ec9807bb9bcd
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-02-15 15:43:48 +0300

    Update V1.py

--- LP9/V1.py
+++ LP9/V1.py
@@ -1,5 +1,10 @@
-import csv
-with open('C:\\Users\\DarkStell\\Documents\\GitHub\\Python24\\LP9\\15 - 1.csv', newline='',encoding='utf-8') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
+import pandas
+a = pandas.read_csv('C:\\Users\\DarkStell\\Documents\\GitHub\\Python24\\LP9\\15 - 1.csv')
+counter = 0
+for i in range(len(a)):
+    time_sec = (a.at["Оценка/10,00"]/100*60 + a.iloc[9]%100)
+    failed_on = (bool(a.iloc[i] and a.iloc[i+1]) for i in range(11,20,2))
+    passed = a.loc[i].at["Оценка/10,00"]
+    if passed and time_sec < 15000 and failed_on:
+        counter +=1
+print(counter)
