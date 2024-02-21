ccb71f057b3525d5c730bb07d37f6364bf008ef4
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-02-21 14:21:37 +0300

    Созданы папки и файлы для остальных изображений

--- LP9/V1.py
+++ LP9/V1.py
@@ -1,10 +1,13 @@
 import pandas
-a = pandas.read_csv('C:\\Users\\DarkStell\\Documents\\GitHub\\Python24\\LP9\\15 - 1.csv')
+df = pandas.read_csv('C:\\Users\\DarkStell\\Documents\\GitHub\\Python24\\LP9\\15 - 1.csv')
 counter = 0
-for i in range(len(a)):
-    time_sec = (a.at["Оценка/10,00"]/100*60 + a.iloc[9]%100)
-    failed_on = (bool(a.iloc[i] and a.iloc[i+1]) for i in range(11,20,2))
-    passed = a.loc[i].at["Оценка/10,00"]
-    if passed and time_sec < 15000 and failed_on:
+for i in range(len(df)):
+    failed_on = any(df.iloc[i,j] == 0 for j in range(10, 20))
+    passed = int(df.iloc[i]["Оценка/10,00"].split(',')[0])>= 6
+    if passed:
+        print(df.iloc[i]["Оценка/10,00"])
+    if failed_on:
+        print(any(df.iloc[i,j] == 0 for j in range(10, 20)))
+    if passed and failed_on:
         counter +=1
 print(counter)
