526ac777fd4d1cef67a318daff01d605d633e2c2
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-02-21 14:34:47 +0300

    bugfix

--- LP9/V1.py
+++ LP9/V1.py
@@ -2,12 +2,12 @@ import pandas
 df = pandas.read_csv('C:\\Users\\DarkStell\\Documents\\GitHub\\Python24\\LP9\\15 - 1.csv')
 counter = 0
 for i in range(len(df)):
-    failed_on = any(df.iloc[i,j] == 0 for j in range(10, 20))
+    themes = [(10, 11), (12, 13), (14, 15), (16, 17), (18, 19)]
+    failed_on = all((df.iloc[i, j].split(',')[0] == '0' and df.iloc[i, k].split(',')[0] == '0') for j, k in themes)
     passed = int(df.iloc[i]["Оценка/10,00"].split(',')[0])>= 6
-    if passed:
-        print(df.iloc[i]["Оценка/10,00"])
-    if failed_on:
-        print(any(df.iloc[i,j] == 0 for j in range(10, 20)))
+  
+
+
     if passed and failed_on:
         counter +=1
 print(counter)
