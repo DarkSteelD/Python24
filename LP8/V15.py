e4de18261ade84695cf7c759d1007ecb3545f050
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-02-22 21:40:36 +0300

    Update V15.py

--- LP8/V15.py
+++ LP8/V15.py
@@ -1,6 +1,14 @@
 import osmium as osm
 import pandas as pd
+import re
 class OSMHandler(osm.SimpleHandler):
     def __init__(self):
         osm.SimpleHandler.__init__(self)
         self.osm_data = []
+osmhandler = OSMHandler()
+osmhandler.apply_file("15.osm")
+# df = pd.read_xml("15.osm")
+#  data_colnames = ['type', 'id', 'version', 'visible', 'ts', 'uid',
+#                   'user', 'chgset', 'ntags', 'tagkey', 'tagvalue']
+# df_osm = pd.DataFrame(osmhandler.osm_data, columns=data_colnames)
+# df_osm = tag_genome.sort_values(by=['type', 'id', 'ts'])
