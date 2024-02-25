7cb7d581382a3cc11c7a965e48b7746dd2dc3b01
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-02-25 11:40:57 +0300

    modified:   LP7/LP7/menu/models.py

--- LP7/LP7/menu/models.py
+++ LP7/LP7/menu/models.py
@@ -4,6 +4,14 @@ class Dish(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    
+    def __str__(self):
+        return self.name
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    working_hours = models.CharField(max_length=255)
+    description = models.TextField()
+
     def __str__(self):
         return self.name
