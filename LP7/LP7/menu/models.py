fd0ffc83670451226c04dd1b836685e7732b0d0c
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-02-25 11:28:31 +0300

    modified:   LP7/LP7/menu/models.py

--- LP7/LP7/menu/models.py
+++ LP7/LP7/menu/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
