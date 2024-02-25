774744be496c7f8c12eab1c0c6f3880635b8e6a8
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-02-25 14:31:15 +0300

    UPD TO LAST VERSION

--- LP8/V15.py
+++ LP8/V15.py
@@ -23,26 +23,6 @@ def main():
         tree = ET.parse('C:\\Users\\DarkStell\\Documents\\GitHub\\Python24\\LP8\\16 - 2.osm')
         root = tree.getroot()
         fuel_stations_by_company, all_fuel_stations = count_fuel_stations(root)
-        
-        ##################
-        for node in root.findall('.//node'):
-            print("Node:", file=file)
-            print("  ID:", node.attrib.get('id'), file=file)
-            print("  Latitude:", node.attrib.get('lat'), file=file)
-            print("  Longitude:", node.attrib.get('lon'), file=file)
-            for tag in node.findall('tag'):
-                print("  Tag:", file=file)
-                print("    Key:", tag.attrib.get('k'), file=file)
-                print("    Value:", tag.attrib.get('v'), file=file)
-
-        for way in root.findall('.//way'):
-            print("Way:", file=file)
-            print("  ID:", way.attrib.get('id'), file=file)
-            for tag in way.findall('tag'):
-                print("  Tag:", file=file)
-                print("    Key:", tag.attrib.get('k'), file=file)
-                print("    Value:", tag.attrib.get('v'), file=file)
-        ##################
         print("Количество заправок по каждой фирме:")
         for company, count in fuel_stations_by_company.items():
             print(f"{company}: {count}")
