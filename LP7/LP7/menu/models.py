10646cab738899e7267609d9e651f669278aadb5
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-02-25 11:56:38 +0300

    Сategory created

--- LP7/LP7/menu/models.py
+++ LP7/LP7/menu/models.py
@@ -4,6 +4,9 @@ class Dish(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    ingredients = models.TextField()
+    category = models.ForeignKey(Category,on_delete = models.CASCADE)
+    photo = models.ImageField(upload_to='dishes/')
     def __str__(self):
         return self.name
 class Restaurant(models.Model):
@@ -12,6 +15,10 @@ class Restaurant(models.Model):
     phone_number = models.CharField(max_length=20)
     working_hours = models.CharField(max_length=255)
     description = models.TextField()
-
     def __str__(self):
         return self.name
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name    
